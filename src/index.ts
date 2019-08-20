6802f10d451f20fdede46cc54d28b9b25e854eff
Wayne Parrott <5588978+wayneparrott@users.noreply.github.com>
2019-08-20 07:58:28 -0500

    added template for using lines.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { GrootClock } from "./groot-clock";
 import { CrookedLinesExample } from "./crooked-lines-example";
 import { Circles } from "./circles";
+import { Lines } from "./lines";
 
 
 const clock = new GrootClock();
 clock.start();
 
 // (new CrookedLinesExample()).start();
-//(new Circles()).start();
+// (new Circles()).start();
+// (new Lines()).start();
 
 
